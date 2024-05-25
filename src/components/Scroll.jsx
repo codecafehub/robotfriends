ebcaa39bcfe3cc0a6ece87cc216e8727c9c61a09
codecafe <ogechiokachi4real@gmail.com>
2024-05-25 16:47:59 +0100

    commit

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
     return (
-        <div>
+        <div className="shadow-5 bt bw1 b--light-silver" style={{overflowY: 'scroll',  height: '500px'}}>
             {props.children}
         </div>
     );
